a9ac31ced80a1741dd55c9b058a3f7cafbffba65
rops3c <37941160+rops3c@users.noreply.github.com>
2020-06-13 03:22:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,33 +7,33 @@ int main(){
 	//Asking user to enter number of the day
 	cout<<"Enter Day of the week: ";
 	//Input DayNum
-	cin>>day;
+	std::cin>>day;
 	switch(day){
 		//Day starts with 0 = Monday
 		case 0:
-			cout<<"Its Monday";
+			std::cout<<"Its Monday";
 			break;
 		case 1:
-			cout<<"Its Tuesday";
+			std::cout<<"Its Tuesday";
 			break;
 		case 2:
-			cout<<"Its Wednesday";
+			std::cout<<"Its Wednesday";
 			break;
 		case 3:
-			cout<<"Its Thursday";
+			std::cout<<"Its Thursday";
 			break;
 		case 4:
-			cout<<"Its Friday";
+			std::cout<<"Its Friday";
 			break;
 		case 5:
-			cout<<"Its saturday";
+			std::cout<<"Its saturday";
 			break;
 		case 6:
-			cout<<"Its sunday";	
+			std::cout<<"Its sunday";	
 			break;
 		//Day 6 = Sunday
 		default:
-			cout<<"Out of week day X:D)";
+			std::cout<<"Out of week day X:D)";
 }
 	
 	return 0;
